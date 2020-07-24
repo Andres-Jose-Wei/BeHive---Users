ff954be083d69e99a502834d2f68e0678538082f
Jose Canela <jose.canela916@gmail.com>
2020-07-24 13:20:31 -0400

    Modified user mapping

--- src/main/java/com/anjowe/behive/controller/UserController.java
+++ src/main/java/com/anjowe/behive/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.anjowe.behive.model.Group;
@@ -56,8 +57,8 @@ public class UserController {
 		return userService.getAllUsers();
 	}
 	
-	@GetMapping("/user/{username}")
-	public Mono<User> getUser(@PathVariable("username") String username){
+	@GetMapping("/user")
+	public Mono<User> getUser(@RequestHeader("USER_DETAILS") String username){
 		return userService.getUser(username);
 	}
 	
