ebb67cd18fa037c50e0e0b6ff0fa98aa8414f423
Jose Canela <jose.canela916@gmail.com>
2020-07-31 10:36:27 -0400

    Added CrossOrigin to SillsController

--- src/main/java/com/anjowe/behive/controller/SkillsController.java
+++ src/main/java/com/anjowe/behive/controller/SkillsController.java
@@ -3,6 +3,7 @@ package com.anjowe.behive.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import com.anjowe.behive.service.SkillsService;
 import reactor.core.publisher.Mono;
 
 @RestController
+@CrossOrigin
 public class SkillsController {
 	private SkillsService skillsService;
 	
